45145927e790d397df53a7c3ef98d4c52f46a121
aprizal darius <aprizaldarius@gmail.com>
2019-06-26 16:34:25 +0700

    Menu Navigasi

---
+++ app/src/main/java/com/marketing/cobanavi/BerandaActivity.java
@@ -0,0 +1,20 @@
+package com.marketing.cobanavi;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BerandaActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmentberanda, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/marketing/cobanavi/MainActivity.java
@@ -0,0 +1,49 @@
+package com.marketing.cobanavi;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new BerandaActivity());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(MainActivity.this);
+    }
+
+    // method untuk load fragment yang sesuai
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment = new BerandaActivity();
+                break;
+            case R.id.rekap_menu:
+                fragment = new RekapActivity();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+}
